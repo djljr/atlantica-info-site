0ea45ac4d07addab6918d2e60826857f8be68175
Den Lipovsky <dennis.lipovsky@gmail.com>
2008-12-13 17:31:54 +0000

    refactoring the sidemenu javascript/css to external files

---
+++ public/js/erenda_lib.js
@@ -0,0 +1,17 @@
+function toggleVisible(elem) {
+    toggleElementClass("invisible", elem);
+}
+
+function makeVisible(elem) {
+    removeElementClass(elem, "invisible");
+}
+
+function makeInvisible(elem) {
+    addElementClass(elem, "invisible");
+}
+
+function isVisible(elem) {
+    // you may also want to check for
+    // getElement(elem).style.display == "none"
+    return !hasElementClass(elem, "invisible");
+}
